2c705f6d93f652ee29c558d876f13783ed5df7cc
Christopher Lamm <chris@theantichris.com>
2020-12-14 06:55:02 -0500

    implemented basic auth

--- main.go
+++ main.go
@@ -18,26 +18,33 @@ func main() {
 	config.BasePath = flagrURL
 	client := goflagr.NewAPIClient(config)
 
+	ctx := context.Background()
+	auth := goflagr.BasicAuth{
+		UserName: "flagr_username",
+		Password: "flagr_password",
+	}
+	ctx = context.WithValue(ctx, goflagr.ContextBasicAuth, auth)
+
 	preload := optional.NewBool(true)
 	params := goflagr.FindFlagsOpts{
 		Preload: preload,
 	}
-	flags, response, err := client.FlagApi.FindFlags(context.Background(), &params)
+	flags, response, err := client.FlagApi.FindFlags(ctx, &params)
 	if err != nil {
 		fmt.Printf("error getting flag: %s\n", err.Error())
 	}
 
-	fmt.Printf("http status: %d\n", response.StatusCode)
+	fmt.Printf("http status: %d\n\n", response.StatusCode)
 
-	fmt.Println("Flag:")
+	fmt.Print("Flag:\n\n")
 	spew.Dump(flags)
 
-	fmt.Println("Flag 2:")
-	spew.Dump(flags[0])
+	// fmt.Println("Flag 2:")
+	// spew.Dump(flags[0])
 
-	fmt.Println("Flag 3:")
-	spew.Dump(flags[1])
+	// fmt.Println("Flag 3:")
+	// spew.Dump(flags[1])
 
-	fmt.Println("Flag 3 Variants:")
-	spew.Dump(flags[1].Variants)
+	// fmt.Println("Flag 3 Variants:")
+	// spew.Dump(flags[1].Variants)
 }
